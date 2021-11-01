46269cfdb2b4e9c7394e8efa16d064d79ef5b010
Zuzka <stetinovaZ@gmail.com>
2021-11-01 19:11:15 +0100

    Initialize - remove useless code.

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Table Data Visualizer</header>
+      <p>Web Application for Table Data Visualization</p>
+    </div>
+  );
+}
+
+export default App;
