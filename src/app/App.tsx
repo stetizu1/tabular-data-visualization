7166054f7be1ccdc5f55bf32545fe95add5a4824
Zuzka <stetinovaZ@gmail.com>
2022-01-10 19:30:36 +0100

    fix data

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,21 @@
 import React, { FunctionComponent } from 'react'
 import { useAppStyle } from './useAppStyle'
 import { BasicScatterPlot } from './scatterplot/BasicScatterPlot'
+import { TestData, testData } from './test/testData'
 
 
 export const App: FunctionComponent = () => {
   const style = useAppStyle()
+  const data = testData
+  const getValueX = (d: TestData) => d.sepalLength
+  const getValueY = (d: TestData) => d.petalLength
+  const getValueCat = (d: TestData) => d.species
 
   return (
     <div className={style.app}>
       <header className={style.appHeader}>Table Data Visualizer</header>
       <p>Web Application for Table Data Visualization</p>
-      {/* todo fix test data */}
-      <BasicScatterPlot width={800} height={512} />
+      <BasicScatterPlot width={800} height={512} dataset={data} getValueX={getValueX} getValueY={getValueY} getValueCat={getValueCat} />
     </div>
   )
 }
--- src/app/scatterplot/BasicScatterPlot.tsx
+++ src/app/scatterplot/BasicScatterPlot.tsx
@@ -1,101 +1,104 @@
-import { FunctionComponent, useCallback, useEffect, useRef } from 'react'
-import { axisBottom, axisLeft, brush, extent, scaleLinear, scaleOrdinal, schemeCategory10, select, brushSelection } from 'd3'
-import { TestData, testData } from '../test/testData'
+import { useCallback, useEffect, useRef } from 'react'
+import {
+  axisBottom,
+  axisLeft,
+  brush,
+  extent,
+  scaleLinear,
+  scaleOrdinal,
+  schemeCategory10,
+  select,
+  brushSelection,
+  ScaleLinear,
+} from 'd3'
 import { Margin, defaultMargin, marginWidth, marginHeight } from '../styles/margin'
 import { useBasicScatterPlotStyle } from './useBasicScatterPlotStyle'
 
-interface ScatterPlotProps {
+interface ScatterPlotProps<T> {
   width: number
   height: number
-  colors?: [string, string]
+  dataset: T[]
+  getValueX: (data: T) => number
+  getValueY: (data: T) => number
+  getValueCat: (data:T) => string
   margin?: Margin
 }
 
 const isBrushed = (brush_coords: [[number, number], [number, number]], cx: number, cy: number) => {
-  const x0 = brush_coords[0][0],
-    x1 = brush_coords[1][0],
-    y0 = brush_coords[0][1],
-    y1 = brush_coords[1][1]
+  const [x0, x1, y0, y1] = [brush_coords[0][0], brush_coords[1][0], brush_coords[0][1], brush_coords[1][1]]
   return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1
 }
 
-export const BasicScatterPlot: FunctionComponent<ScatterPlotProps> = ({
+const addAxes = (node: SVGGElement, xScale: ScaleLinear<number, number>, yScale: ScaleLinear<number, number>, innerHeight: number) => {
+  const [xAxisG, yAxisG] = [
+    select(node).append(`g`).attr(`transform`, `translate(0,${innerHeight})`),
+    select(node).append(`g`),
+  ]
+  const [xAxis, yAxis] = [axisBottom(xScale), axisLeft(yScale)]
+  xAxisG.call(xAxis)
+  yAxisG.call(yAxis)
+}
+
+export const BasicScatterPlot = <T, >({
   width,
   height,
+  dataset, getValueX, getValueY, getValueCat,
   margin = defaultMargin,
-}) => {
+}: ScatterPlotProps<T>) => {
   const style = useBasicScatterPlotStyle()
 
   const component = useRef<SVGGElement>(null)
   const [innerWidth, innerHeight] = [width - marginWidth(margin), height - marginHeight(margin)]
 
-  // todo to parameters
-  const data = testData
-  const xValue = (d: TestData) => d.sepalLength
-  const yValue = (d: TestData) => d.petalLength
-  const catValue = (d: TestData) => d.species
-
-  const createBarChart = useCallback(() => {
+  const createScatterPlot = useCallback(() => {
     const node = component.current
     if (!node) {
       return
     }
-    const color = scaleOrdinal(schemeCategory10)
-    const fill = (d: TestData) => color(catValue(d))
-
-    const [xAxisG, yAxisG] = [
-      select(node).append(`g`).attr(`transform`, `translate(0,` + innerHeight + `)`),
-      select(node).append(`g`),
-    ]
-
-    const [xExtent, yExtent] = [extent(data, xValue), extent(data, yValue)]
-    if (!xExtent[0] || !yExtent[0]) { // [undefined, undefined]
-      return
-    }
-    const [xScale, yScale] = [scaleLinear(), scaleLinear()]
-    const [xAxis, yAxis] = [axisBottom(xScale), axisLeft(yScale)]
-
-    xScale
-      .domain(xExtent)
-      .range([0, innerWidth])
-    yScale
-      .domain(yExtent)
-      .range([innerHeight, 0])
     const circles = select(node)
       .selectAll(`circle`)
-      .data(data)
+      .data(dataset)
       .enter()
       .append(`circle`)
 
+    const [xExtent, yExtent] = [extent(dataset, getValueX), extent(dataset, getValueY)]
+    if (!xExtent[0] || !yExtent[0]) // [undefined, undefined]
+      return
+    const [xScale, yScale] = [
+      scaleLinear().domain(xExtent).range([0, innerWidth]),
+      scaleLinear().domain(yExtent).range([innerHeight, 0]),
+    ]
+    const fill = (data: T) => scaleOrdinal(schemeCategory10)(getValueCat(data))
     circles
-      .attr(`cx`, d => xScale(xValue(d)))
-      .attr(`cy`, d => yScale(yValue(d)))
+      .attr(`cx`, d => xScale(getValueX(d)))
+      .attr(`cy`, d => yScale(getValueY(d)))
       .attr(`r`, 3)
       .attr(`fill`, fill)
-      .attr(`opacity`, 0.1)
+      .attr(`opacity`, 0.25)
+
+    addAxes(node, xScale, yScale, innerHeight)
 
     const startBrushing = () => {
       const selection = brushSelection(node)
       if (selection) {
         const extent = selection as [[number, number], [number, number]] // hard retype
-        circles.classed(style.selected, (d: TestData) => {
-          return isBrushed(extent, xScale(d.sepalLength), yScale(d.petalLength))
+        circles.classed(style.selected, (d: T) => {
+          return isBrushed(extent, xScale(getValueX(d)), yScale(getValueY(d)))
         },
         )
       }
     }
-
     select(node).call(
       brush()
         .extent([[0, -margin.top], [innerWidth, innerHeight]])
         .on(`start brush`, startBrushing),
     )
+  }, [
+    dataset, getValueX, getValueY, getValueCat,
+    innerHeight, innerWidth, component, margin, style,
+  ])
 
-    xAxisG.call(xAxis)
-    yAxisG.call(yAxis)
-  }, [data, innerHeight, innerWidth, component, margin, style])
-
-  useEffect(() => createBarChart())
+  useEffect(() => createScatterPlot())
   return <>
     <svg width={width} height={height} className={style.svg}>
       <g ref={component} transform={`translate(${margin.left}, ${margin.top})`}/>
